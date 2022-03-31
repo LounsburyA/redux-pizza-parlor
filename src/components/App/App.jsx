0c2761916faba1456a624ea5275db679d9a736d8
Andrew Lounsbury <lounsburya@gmail.com>
2022-03-31 10:19:40 -0500

    set up project, add GET route for pizzas in app

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,8 +1,29 @@
 import React from 'react';
 import axios from 'axios';
 import './App.css';
+import {useState, useEffect} from 'react';
+
+import {useDispatch} from 'react-redux';
 
 function App() {
+  const dispatch = useDispatch();
+  useEffect(()=>{
+    console.log('in useEffect');
+    getPizza();
+  }, []);
+
+const getPizza =()=>{
+axios.get('/api/pizza')
+.then((response)=> {
+console.log(response.data);
+dispatch({type: 'SET_PIZZA_LIST', payload: response.data})
+})
+.catch((error)=>{
+  console.log('error in GET pizza', error);
+});
+
+};
+  
 
   return (
     <div className='App'>
--- src/index.js
+++ src/index.js
@@ -2,5 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
+import {Provider} from 'react-redux';
+import {createStore, combineReducers, applyMiddleware} from 'redux';
+import logger from 'redux-logger'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+const pizzaReducer = (state = [], action) => {
+    if(action.type === 'SET_PIZZA_LIST'){
+        return action.payload
+    }
+    return state
+}
+
+const storeInstance = createStore(
+    combineReducers({
+        pizzaReducer,
+    }),
+    applyMiddleware(logger),
+);
+
+ReactDOM.render(<Provider store={storeInstance}><App /></Provider>, document.getElementById('root'));
